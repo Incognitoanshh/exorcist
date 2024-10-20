728034b8a21baf0115b83748693cdec70d773ee9
Incognitoanshh <amritanshu1403@gmail.com>
2024-10-20 21:17:30 +0530

    Updated HTML and added Bootstrap CDN

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"; // Change this import
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Create a root
 const root = ReactDOM.createRoot(document.getElementById("root"));
